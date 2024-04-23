556b441d676f6d2b0eea0eacebdc711a53b04e3f
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-04-23 22:54:35 +0200

    Add Temperature sensor

--- cooling_system.ino
+++ cooling_system.ino
@@ -1,11 +1,18 @@
+#include <DHT.h> // Including library for dht
 #include <ESP8266WiFi.h>
 
-#define WIFI_SSID "Your Wifi SSID"
-#define WIFI_PASSWORD "Your Wifi Password"
+#define WIFI_SSID "D-Link"
+#define WIFI_PASSWORD "HarryPotter24"
+
+#define DHTPIN 2 // pin where the dht11 is connected
+
+DHT dht(DHTPIN, DHT11);
 
 void setup()
 {
   Serial.begin(115200);
+  delay(10);
+  dht.begin();
 
   Serial.println("Connecting to ");
   Serial.println(WIFI_SSID);
@@ -26,4 +33,16 @@ void setup()
 
 void loop()
 {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" degrees Celcius, Humidity: ");
+  Serial.print(h);
 }
