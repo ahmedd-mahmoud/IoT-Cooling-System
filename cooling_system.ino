69161a0a82ef1805af0c15d1676002f1f56d9702
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-04-23 22:57:40 +0200

    Integrate Thingspeak with microcontroller

--- cooling_system.ino
+++ cooling_system.ino
@@ -1,12 +1,18 @@
 #include <DHT.h> // Including library for dht
 #include <ESP8266WiFi.h>
 
-#define WIFI_SSID "D-Link"
-#define WIFI_PASSWORD "HarryPotter24"
+String apiKey = "NMAHB3FL4DO0LQ1W"; //  Enter your Write API key from ThingSpeak
+
+#define WIFI_SSID "Your Wifi SSID"
+#define WIFI_PASSWORD "Your Wifi Password"
+
+const char *server = "api.thingspeak.com";
 
 #define DHTPIN 2 // pin where the dht11 is connected
 
 DHT dht(DHTPIN, DHT11);
+
+WiFiClient thingClient;
 
 void setup()
 {
@@ -29,20 +35,48 @@ void setup()
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-}
 
-void loop()
-{
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  if (isnan(h) || isnan(t))
+  void loop()
   {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t))
+    {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    if (thingClient.connect(server, 80)) //   "184.106.153.149" or api.thingspeak.com
+    {
+
+      String postStr = apiKey;
+      postStr += "&field1=";
+      postStr += String(t);
+      postStr += "&field2=";
+      postStr += String(h);
+      postStr += "\r\n\r\n";
+
+      thingClient.print("POST /update HTTP/1.1\n");
+      thingClient.print("Host: api.thingspeak.com\n");
+      thingClient.print("Connection: close\n");
+      thingClient.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+      thingClient.print("Content-Type: application/x-www-form-urlencoded\n");
+      thingClient.print("Content-Length: ");
+      thingClient.print(postStr.length());
+      thingClient.print("\n\n");
+      thingClient.print(postStr);
+
+      Serial.print("Temperature: ");
+      Serial.print(t);
+      Serial.print(" degrees Celcius, Humidity: ");
+      Serial.print(h);
+      Serial.println("%. Send to Thingspeak.");
+    }
+    thingClient.stop();
+
+    Serial.println("Waiting...");
+
+    // thingspeak needs minimum 15 sec delay between updates
+    delay(1000);
   }
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" degrees Celcius, Humidity: ");
-  Serial.print(h);
-}
