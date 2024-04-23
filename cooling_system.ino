90541e579e24b383082abc7f2cb11d86be3182f1
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-04-23 22:59:30 +0200

    Integrate Hive MQ cloud with microcontroller

--- cooling_system.ino
+++ cooling_system.ino
@@ -1,10 +1,18 @@
 #include <DHT.h> // Including library for dht
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <WifiClientSecure.h>
 
 String apiKey = "NMAHB3FL4DO0LQ1W"; //  Enter your Write API key from ThingSpeak
 
 #define WIFI_SSID "Your Wifi SSID"
 #define WIFI_PASSWORD "Your Wifi Password"
+
+#define mqtt_server "1ec4d4cdb4ed402694590af113b150f6.s1.eu.hivemq.cloud"
+#define mqtt_username "esp8266"
+#define mqtt_password "Esp8266cooling"
+#define mqtt_port 8883
 
 const char *server = "api.thingspeak.com";
 
@@ -12,7 +20,92 @@ const char *server = "api.thingspeak.com";
 
 DHT dht(DHTPIN, DHT11);
 
+WiFiClientSecure espClient;
+
 WiFiClient thingClient;
+
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg(MSG_BUFFER_SIZE);
+
+// static const char *root_ca PROGMEM = R"EOF(
+//   -----BEGIN CERTIFICATE-----
+// MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+// TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+// cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+// WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+// ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+// MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+// h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+// 0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+// A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+// T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+// B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+// B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+// KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+// OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+// jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+// qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+// rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+// HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+// hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+// ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+// 3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+// NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+// ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+// TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+// jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+// oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+// 4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+// mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+// emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+// -----END CERTIFICATE-----
+// )EOF";
+
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-mahran";
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password))
+    {
+      Serial.println("connected");
+      client.subscribe("temp");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.print("try again in 5 sec");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  String incommingMsg = "";
+  for (int i = 0; i < length; i++)
+    incommingMsg += (char)payload[i];
+
+  Serial.print("Message arrived [" + String(topic) + "]" + incommingMsg);
+
+  // check the incomming msg
+  // if(strcmp(topic,"temp") == 0){
+
+  // }
+}
+
+void publishMsg(const char *topic, String payload)
+{
+  if (client.publish(topic, payload.c_str(), true))
+    Serial.println("Message published [" + String(topic) + "]" + payload);
+}
 
 void setup()
 {
@@ -36,47 +129,75 @@ void setup()
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  void loop()
+  espClient.setInsecure();
+  // espClient.setCACert(root_ca);
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+  if (!client.connected())
+    reconnect();
+  client.loop();
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t))
   {
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-
-    if (isnan(h) || isnan(t))
-    {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
-
-    if (thingClient.connect(server, 80)) //   "184.106.153.149" or api.thingspeak.com
-    {
-
-      String postStr = apiKey;
-      postStr += "&field1=";
-      postStr += String(t);
-      postStr += "&field2=";
-      postStr += String(h);
-      postStr += "\r\n\r\n";
-
-      thingClient.print("POST /update HTTP/1.1\n");
-      thingClient.print("Host: api.thingspeak.com\n");
-      thingClient.print("Connection: close\n");
-      thingClient.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
-      thingClient.print("Content-Type: application/x-www-form-urlencoded\n");
-      thingClient.print("Content-Length: ");
-      thingClient.print(postStr.length());
-      thingClient.print("\n\n");
-      thingClient.print(postStr);
-
-      Serial.print("Temperature: ");
-      Serial.print(t);
-      Serial.print(" degrees Celcius, Humidity: ");
-      Serial.print(h);
-      Serial.println("%. Send to Thingspeak.");
-    }
-    thingClient.stop();
-
-    Serial.println("Waiting...");
-
-    // thingspeak needs minimum 15 sec delay between updates
-    delay(1000);
+    Serial.println("Failed to read from DHT sensor!");
+    return;
   }
+
+  DynamicJsonDocument tempDoc(1024);
+  DynamicJsonDocument humDoc(1024);
+
+  humDoc["humidity"] = h;
+  tempDoc["temperature"] = t;
+
+  char temp_message[128];
+  char hum_message[128];
+
+  serializeJson(tempDoc, temp_message);
+  serializeJson(humDoc, hum_message);
+
+  publishMsg("temp", temp_message);
+  publishMsg("hum", hum_message);
+
+  delay(1000);
+
+  if (thingClient.connect(server, 80)) //   "184.106.153.149" or api.thingspeak.com
+  {
+
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(t);
+    postStr += "&field2=";
+    postStr += String(h);
+    postStr += "\r\n\r\n";
+
+    thingClient.print("POST /update HTTP/1.1\n");
+    thingClient.print("Host: api.thingspeak.com\n");
+    thingClient.print("Connection: close\n");
+    thingClient.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    thingClient.print("Content-Type: application/x-www-form-urlencoded\n");
+    thingClient.print("Content-Length: ");
+    thingClient.print(postStr.length());
+    thingClient.print("\n\n");
+    thingClient.print(postStr);
+
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" degrees Celcius, Humidity: ");
+    Serial.print(h);
+    Serial.println("%. Send to Thingspeak.");
+  }
+  thingClient.stop();
+
+  Serial.println("Waiting...");
+
+  // thingspeak needs minimum 15 sec delay between updates
+  delay(1000);
+}
